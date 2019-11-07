29dd58d8902eab9e2526b5c9c97e74bc0170176f
brandenlacour <brandenlacour@gmail.com>
2019-11-07 11:14:00 -0600

    new thing

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
 
-console.log("user flipped" + cardOne);
-console.log("user flipped" + cardTwo);
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay[0] === cardsInPlay[1]) {
+  alert("you found a match!");
+} else {
+  alert("sorry try again");
+}
