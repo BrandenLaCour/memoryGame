320cb5549405cb0b230bf7d700997c19eaf8d6eb
brandenlacour <brandenlacour@gmail.com>
2019-11-08 10:19:33 -0600

    added card flip

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+  { rank: "queen", suit: "hearts", cardImage: "images/queen-of-hearts.png" },
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+  },
+  { rank: "king", suit: "hearts", cardImage: "images/king-of-hearts.png" },
+  { rank: "king", suit: "diamonds", cardImage: "images/king-of-diamonds.png" }
+];
 
 var cardsInPlay = [];
 
@@ -10,12 +19,28 @@ function checkForMatch() {
   }
 }
 
-function flipCard(cardId) {
-  console.log("user flipped" + cards[cardId]);
+function flipCard() {
+  var cardId = this.getAttribute("data-id");
 
-  cardsInPlay.push(cards[cardId]);
+  cardsInPlay.push(cards[cardId].rank);
+
+  if (cardsInPlay.length === 1) {
+    this.setAttribute("src", cards[cardId].cardImage);
+    console.log(cardsInPlay);
+  } else if (cardsInPlay.length > 1) {
+    this.setAttribute("src", cards[cardId].cardImage);
+    setTimeout(checkForMatch, 200);
+  }
 }
 
-flipCard(0);
-flipCard(1);
-checkForMatch();
+function createBoard() {
+  for (let i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement("img");
+    cardElement.setAttribute("src", "images/back.png");
+    cardElement.setAttribute("data-id", i);
+    cardElement.addEventListener("click", flipCard);
+    document.querySelector("#game-board").appendChild(cardElement);
+  }
+}
+
+createBoard();
